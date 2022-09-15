07e392f66fce5c3239221b46f9fb4f803f312ac9
Schroeder Chicora Alexandre <alexandre.schroeder-chicora@insa-rennes.fr>
2022-09-15 14:05:21 +0200

    updating new test files for RDUnet model

--- model.py
+++ model.py
@@ -266,6 +266,90 @@ class DenoisingBlock(nn.Module):
 
         return out_3 + x
 
+class RDUNet(nn.Module):
+    r"""
+    Residual-Dense U-net for image denoising.
+    """
+    def __init__(self):
+        super().__init__()
+
+        channels = 3
+        filters_0 = 128
+        filters_1 = 2 * filters_0
+        filters_2 = 4 * filters_0
+        filters_3 = 8 * filters_0
+
+        # Encoder:
+        # Level 0:
+        self.input_block = InputBlock(channels, filters_0)
+        self.block_0_0 = DenoisingBlock(filters_0, filters_0 // 2, filters_0)
+        self.block_0_1 = DenoisingBlock(filters_0, filters_0 // 2, filters_0)
+        self.down_0 = DownsampleBlock(filters_0, filters_1)
+
+        # Level 1:
+        self.block_1_0 = DenoisingBlock(filters_1, filters_1 // 2, filters_1)
+        self.block_1_1 = DenoisingBlock(filters_1, filters_1 // 2, filters_1)
+        self.down_1 = DownsampleBlock(filters_1, filters_2)
+
+        # Level 2:
+        self.block_2_0 = DenoisingBlock(filters_2, filters_2 // 2, filters_2)
+        self.block_2_1 = DenoisingBlock(filters_2, filters_2 // 2, filters_2)
+        self.down_2 = DownsampleBlock(filters_2, filters_3)
+
+        # Level 3 (Bottleneck)
+        self.block_3_0 = DenoisingBlock(filters_3, filters_3 // 2, filters_3)
+        self.block_3_1 = DenoisingBlock(filters_3, filters_3 // 2, filters_3)
+
+        # Decoder
+        # Level 2:
+        self.up_2 = UpsampleBlock(filters_3, filters_2, filters_2)
+        self.block_2_2 = DenoisingBlock(filters_2, filters_2 // 2, filters_2)
+        self.block_2_3 = DenoisingBlock(filters_2, filters_2 // 2, filters_2)
+
+        # Level 1:
+        self.up_1 = UpsampleBlock(filters_2, filters_1, filters_1)
+        self.block_1_2 = DenoisingBlock(filters_1, filters_1 // 2, filters_1)
+        self.block_1_3 = DenoisingBlock(filters_1, filters_1 // 2, filters_1)
+
+        # Level 0:
+        self.up_0 = UpsampleBlock(filters_1, filters_0, filters_0)
+        self.block_0_2 = DenoisingBlock(filters_0, filters_0 // 2, filters_0)
+        self.block_0_3 = DenoisingBlock(filters_0, filters_0 // 2, filters_0)
+
+        self.output_block = OutputBlock(filters_0, channels)
+
+    def forward(self, inputs):
+        out_0 = self.input_block(inputs)    # Level 0
+        out_0 = self.block_0_0(out_0)
+        out_0 = self.block_0_1(out_0)
+
+        out_1 = self.down_0(out_0)          # Level 1
+        out_1 = self.block_1_0(out_1)
+        out_1 = self.block_1_1(out_1)
+
+        out_2 = self.down_1(out_1)          # Level 2
+        out_2 = self.block_2_0(out_2)
+        out_2 = self.block_2_1(out_2)
+
+        out_3 = self.down_2(out_2)          # Level 3 (Bottleneck)
+        out_3 = self.block_3_0(out_3)
+        out_3 = self.block_3_1(out_3)
+
+        out_4 = self.up_2([out_3, out_2])   # Level 2
+        out_4 = self.block_2_2(out_4)
+        out_4 = self.block_2_3(out_4)
+
+        out_5 = self.up_1([out_4, out_1])   # Level 1
+        out_5 = self.block_1_2(out_5)
+        out_5 = self.block_1_3(out_5)
+
+        out_6 = self.up_0([out_5, out_0])   # Level 0
+        out_6 = self.block_0_2(out_6)
+        out_6 = self.block_0_3(out_6)
+
+        return self.output_block(out_6) + inputs
+
+
 
 class DyNNet(nn.Module):
 
